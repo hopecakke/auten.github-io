55d816b588deabc0852551f15f9cd83352b6b478
ANA DOROTEO <94026358+hopecakke@users.noreply.github.com>
2021-11-25 19:21:52 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,5 +1,11 @@
-/*  Initializa Firebase con la configuración del proyecto. Revisa la
- * configuración en tu servidor de Firebase.  */
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
 // @ts-ignore
 firebase.initializeApp({
   apiKey: "AIzaSyDRsaSa7LkimyjlAEvfjZpBX2Qe_zA8gPU",
@@ -9,4 +15,4 @@ firebase.initializeApp({
   messagingSenderId: "683481567401",
   appId: "1:683481567401:web:0557c47b57021e98fe59b8",
   measurementId: "G-P3RFYSJNY3"
-    });
+});
