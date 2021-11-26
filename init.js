5402c64bb93cfc963f48469dd4dd7a43d10aacaa
ANA DOROTEO <94026358+hopecakke@users.noreply.github.com>
2021-11-25 19:10:33 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,8 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,6 +11,3 @@ const firebaseConfig = {
   measurementId: "G-P3RFYSJNY3"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
