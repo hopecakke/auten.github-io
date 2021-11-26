b02c4f7b22c5bbf165237786e370482d8ab3d82b
ANA DOROTEO <94026358+hopecakke@users.noreply.github.com>
2021-11-25 19:11:09 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,7 +1,6 @@
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// @ts-ignore
+firebase.initializeApp({
   apiKey: "AIzaSyDRsaSa7LkimyjlAEvfjZpBX2Qe_zA8gPU",
   authDomain: "proyec-auten.firebaseapp.com",
   projectId: "proyec-auten",
@@ -9,5 +8,5 @@ const firebaseConfig = {
   messagingSenderId: "683481567401",
   appId: "1:683481567401:web:0557c47b57021e98fe59b8",
   measurementId: "G-P3RFYSJNY3"
-};
+    });
 
